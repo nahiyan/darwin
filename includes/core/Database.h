7a979268aa535c314996b5a71aec39169c952f62
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-10-20 12:07:57 +0600

    Add features to database library.
    Former-commit-id: ae5824da08f93b6f353a68564cc8c4a860f07a4c

--- includes/core/Database.h
+++ includes/core/Database.h
@@ -4,6 +4,7 @@
 #include <sqlite3/sqlite3.h>
 #include <cstdio>
 #include <string>
+#include <vector>
 
 void sqlite_error()
 {
@@ -34,21 +35,94 @@ public:
 
         sqlite3_stmt *statement;
         if (sqlite3_prepare_v2(Database::handle, sql.c_str(), -1, &statement, 0) == SQLITE_OK)
-            sqlite3_bind_text(statement, 1, name.c_str(), -1, SQLITE_STATIC);
-        else
-            sqlite_error();
-
-        if (sqlite3_step(statement) == SQLITE_ROW)
         {
-            int id = sqlite3_column_int(statement, 0);
-            sqlite3_finalize(statement);
+            sqlite3_bind_text(statement, 1, name.c_str(), -1, SQLITE_STATIC);
 
-            return id;
+            if (sqlite3_step(statement) == SQLITE_ROW)
+            {
+                int id = sqlite3_column_int(statement, 0);
+                sqlite3_finalize(statement);
+
+                return id;
+            }
+            else
+            {
+                return 0;
+            }
         }
         else
         {
+            sqlite_error();
             return 0;
         }
+    }
+
+    static std::vector<std::string> getExtensionNames()
+    {
+        std::string sql = "SELECT name FROM extensions WHERE 1";
+        std::vector<std::string> names;
+
+        sqlite3_stmt *statement;
+        if (sqlite3_prepare_v2(Database::handle, sql.c_str(), -1, &statement, 0) == SQLITE_OK)
+        {
+            while (sqlite3_step(statement) == SQLITE_ROW)
+                names.push_back(std::string((const char *)sqlite3_column_text(statement, 0)));
+
+            sqlite3_finalize(statement);
+        }
+        else
+        {
+            sqlite_error();
+        }
+
+        return names;
+    }
+
+    static std::vector<int> getSessionIds(std::string extensionName)
+    {
+        std::string sql = "SELECT s.id FROM extensions e LEFT JOIN sessions s ON e.id = s.extension_id WHERE e.name = ? ORDER BY s.id DESC";
+        std::vector<int> ids;
+
+        sqlite3_stmt *statement;
+        if (sqlite3_prepare_v2(Database::handle, sql.c_str(), -1, &statement, 0) == SQLITE_OK)
+        {
+            sqlite3_bind_text(statement, 1, extensionName.c_str(), -1, SQLITE_STATIC);
+
+            while (sqlite3_step(statement) == SQLITE_ROW)
+                ids.push_back(sqlite3_column_int(statement, 0));
+
+            sqlite3_finalize(statement);
+        }
+        else
+        {
+            sqlite_error();
+        }
+
+        return ids;
+    }
+
+    static std::vector<int> getGenerationIds(std::string extensionName, int sessionId)
+    {
+        std::string sql = "SELECT g.id FROM extensions e LEFT JOIN sessions s ON e.id = s.extension_id LEFT JOIN generations g ON g.session_id = s.id WHERE e.name = ? AND s.id = ? ORDER BY g.id DESC";
+        std::vector<int> ids;
+
+        sqlite3_stmt *statement;
+        if (sqlite3_prepare_v2(Database::handle, sql.c_str(), -1, &statement, 0) == SQLITE_OK)
+        {
+            sqlite3_bind_text(statement, 1, extensionName.c_str(), -1, SQLITE_STATIC);
+            sqlite3_bind_int(statement, 2, sessionId);
+
+            while (sqlite3_step(statement) == SQLITE_ROW)
+                ids.push_back(sqlite3_column_int(statement, 0));
+
+            sqlite3_finalize(statement);
+        }
+        else
+        {
+            sqlite_error();
+        }
+
+        return ids;
     }
 
     static int addSession(int extensionId)
