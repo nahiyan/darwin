4386a0146f6963832e801a2948ad49486965d713
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-12-29 19:09:52 +0600

    Blank
    Former-commit-id: 0c09a642343a164f873f9e7ef43a1ad244fd54f4

--- core/src/CoreSession.cpp
+++
@@ -1,8 +0,0 @@
-#include <core/CoreSession.h>
-
-int CoreSession::extensionId = 0;
-int CoreSession::sessionId = 0;
-int CoreSession::generationId = 0;
-int CoreSession::generationIndex = 1;
-float CoreSession::speed = 1;
-CoreSession::HUDSelection CoreSession::hudSelection = MutationRate;
