ad06ef0810bbfeb20a1b5d95b8aad37ae23e157b
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-12-31 15:31:14 +0600

    Unify flatbuffers common tables as a part of the "Core" in the portal.
    Former-commit-id: a3b5ce6a6a3f3eff8f5bc475d3f6a78216038db0

--- portal/routes/api.php
+++ portal/routes/api.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Extension;
 use App\Models\Generation;
 use App\Models\Session;
-use Jumper\GenerationState as JumperGenerationState;
-use Wheels\GenerationState as WheelsGenerationState;
-use Flappers\GenerationState as FlappersGenerationState;
+use Core\GenerationState as CoreGenerationState;
 use Google\FlatBuffers\ByteBuffer;
 use Illuminate\Support\Facades\Log;
 
@@ -51,43 +49,15 @@ Route::get('/generation_scores/{generationId}', function ($generationId) {
     $extensionName = $generation->session->extension->name;
 
     $scores = [];
-    switch ($extensionName) {
-        case "Jumper":
-            $byteBuffer = new ByteBuffer(strlen($stateBinary));
-            for ($i = 0; $i < strlen($stateBinary); $i++) {
-                $byteBuffer->put($i, $stateBinary[$i]);
-            }
+    $byteBuffer = new ByteBuffer(strlen($stateBinary));
+    for ($i = 0; $i < strlen($stateBinary); $i++) {
+        $byteBuffer->put($i, $stateBinary[$i]);
+    }
 
-            $generationState = JumperGenerationState::getRootAsGenerationState($byteBuffer);
+    $generationState = CoreGenerationState::getRootAsGenerationState($byteBuffer);
 
-            for ($i = 0; $i < $generationState->getPopulationLength(); $i++) {
-                $scores[] = $generationState->getPopulation($i)->getScore();
-            }
-            break;
-        case "Wheels":
-            $byteBuffer = new ByteBuffer(strlen($stateBinary));
-            for ($i = 0; $i < strlen($stateBinary); $i++) {
-                $byteBuffer->put($i, $stateBinary[$i]);
-            }
-
-            $generationState = WheelsGenerationState::getRootAsGenerationState($byteBuffer);
-
-            for ($i = 0; $i < $generationState->getPopulationLength(); $i++) {
-                $scores[] = $generationState->getPopulation($i)->getScore();
-            }
-            break;
-        case "Flappers":
-            $byteBuffer = new ByteBuffer(strlen($stateBinary));
-            for ($i = 0; $i < strlen($stateBinary); $i++) {
-                $byteBuffer->put($i, $stateBinary[$i]);
-            }
-
-            $generationState = FlappersGenerationState::getRootAsGenerationState($byteBuffer);
-
-            for ($i = 0; $i < $generationState->getPopulationLength(); $i++) {
-                $scores[] = $generationState->getPopulation($i)->getScore();
-            }
-            break;
+    for ($i = 0; $i < $generationState->getPopulationLength(); $i++) {
+        $scores[] = $generationState->getPopulation($i)->getScore();
     }
 
     rsort($scores);
@@ -106,51 +76,19 @@ Route::get('/session_scores/{sessionId}', function ($sessionId) {
         $stateBinary = $generations[$i]->state;
 
         $generation_scores_average = 0;
-        switch ($generations[$i]->session->extension->name) {
-            case "Jumper":
-                $byteBuffer = new ByteBuffer(strlen($stateBinary));
-                for ($j = 0; $j < strlen($stateBinary); $j++) {
-                    $byteBuffer->put($j, $stateBinary[$j]);
-                }
 
-                $generationState = JumperGenerationState::getRootAsGenerationState($byteBuffer);
-
-                for ($k = 0; $k < $generationState->getPopulationLength(); $k++) {
-                    $generation_scores_average += $generationState->getPopulation($k)->getScore();
-                }
-
-                $generation_scores_average /= $generationState->getPopulationLength();
-
-                break;
-            case "Wheels":
-                $byteBuffer = new ByteBuffer(strlen($stateBinary));
-                for ($j = 0; $j < strlen($stateBinary); $j++) {
-                    $byteBuffer->put($j, $stateBinary[$j]);
-                }
-
-                $generationState = WheelsGenerationState::getRootAsGenerationState($byteBuffer);
-
-                for ($k = 0; $k < $generationState->getPopulationLength(); $k++) {
-                    $generation_scores_average += $generationState->getPopulation($k)->getScore();
-                }
-
-                $generation_scores_average /= $generationState->getPopulationLength();
-                break;
-            case "Flappers":
-                $byteBuffer = new ByteBuffer(strlen($stateBinary));
-                for ($j = 0; $j < strlen($stateBinary); $j++) {
-                    $byteBuffer->put($j, $stateBinary[$j]);
-                }
-
-                $generationState = FlappersGenerationState::getRootAsGenerationState($byteBuffer);
-
-                for ($k = 0; $k < $generationState->getPopulationLength(); $k++) {
-                    $generation_scores_average += $generationState->getPopulation($k)->getScore();
-                }
-
-                $generation_scores_average /= $generationState->getPopulationLength();
-                break;
+        $byteBuffer = new ByteBuffer(strlen($stateBinary));
+        for ($j = 0; $j < strlen($stateBinary); $j++) {
+            $byteBuffer->put($j, $stateBinary[$j]);
         }
+
+        $generationState = CoreGenerationState::getRootAsGenerationState($byteBuffer);
+
+        for ($k = 0; $k < $generationState->getPopulationLength(); $k++) {
+            $generation_scores_average += $generationState->getPopulation($k)->getScore();
+        }
+
+        $generation_scores_average /= $generationState->getPopulationLength();
 
         $session_scores[] = $generation_scores_average;
     }
