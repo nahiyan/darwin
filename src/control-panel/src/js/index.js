b98d71d8b85ccb4c653eaf8c5892861c5fcb93f7
nahiyan <nahiyan.alamgir0@gmail.com>
2021-04-12 21:12:03 +0600

    Remove unnecessary files.
    Former-commit-id: 32b5bbd640066ac5ed1fcae6a673f74c723c2ff6

--- src/control-panel/src/js/index.js
+++ src/control-panel/src/js/index.js
@@ -1,22 +1,22 @@
 import '../sass/base.sass'
 import { Elm } from '../elm/src/Main.elm'
 import '@fortawesome/fontawesome-free/js/all'
-// import { exec } from 'child_process'
-
+import { exec } from 'child_process'
+const os = require('os')
 const path = require('path')
 const toml = require('toml')
 const fs = require('fs')
 
-function prepareConfig (extension, config, initialModels) {
+function prepareConfig (extensionName, config, generateRandomModels) {
   const preparedConfig = {}
 
-  if (config[extension] === undefined) { config[extension] = {} }
+  if (config[extensionName] === undefined) { config[extensionName] = {} }
 
-  Object.keys(config.common).concat(Object.keys(config[extension])).forEach(function (key) {
-    preparedConfig[key] = config[extension][key] === undefined ? config.common[key] : config[extension][key]
+  Object.keys(config.common).concat(Object.keys(config[extensionName])).forEach(function (key) {
+    preparedConfig[key] = config[extensionName][key] === undefined ? config.common[key] : config[extensionName][key]
   })
 
-  preparedConfig.initial_models = initialModels
+  preparedConfig.models_file_path = generateRandomModels === true ? path.resolve('..', '..', 'models', extensionName + '.json') : ''
 
   return preparedConfig
 }
@@ -30,23 +30,26 @@ window.addEventListener('DOMContentLoaded', () => {
 
     app.ports.startExtension.subscribe(function (info) {
       const extensionName = info[0]
-      const initialModels = info[1]
+      const generateRandomModels = info[1] === 'random'
 
-      const preparedConfig = JSON.stringify(prepareConfig(extensionName, config, initialModels))
+      const preparedConfig = JSON.stringify(prepareConfig(extensionName, config, generateRandomModels)).replace(/"+/g, '\\"')
+
+      let binaryPath = path.resolve('..', '..', 'extensions', 'bin', extensionName)
+      if (os.platform() === 'darwin') { binaryPath = path.join(binaryPath, 'Contents', 'MacOS', extensionName) }
 
       console.log(preparedConfig)
 
-      // exec(`../../extensions/bin/${extensionName} "${preparedConfig}"`, function (error, stdout, stderr) {
-      //   if (error) {
-      //     console.log(`error: ${error.message}`)
-      //     return
-      //   }
-      //   if (stderr) {
-      //     console.log(`stderr: ${stderr}`)
-      //     return
-      //   }
-      //   console.log(stdout)
-      // })
+      exec(`${binaryPath} "${preparedConfig}"`, function (error, stdout, stderr) {
+        if (error) {
+          console.log(`error: ${error.message}`)
+          return
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`)
+          return
+        }
+        console.log(stdout)
+      })
     })
   } catch (e) {
     console.error('Parsing error on line ' + e.line + ', column ' + e.column +
