4f1e87b84c68ec8c88fc63992563a8e9e3a3dc80
MomenaAkhter <makhter.shukhi@gmail.com>
2021-06-04 21:19:03 +0600

    Pong: Set maximum velocity for ball and enable CCD.
    Former-commit-id: 8b3b869f6e66730a0e78119df9f5d2e1b39a4396

--- extensions/pong/scripts/Ball.gd
+++ extensions/pong/scripts/Ball.gd
@@ -1,1 +1,9 @@
 extends RigidBody2D
+
+var max_speed = 424
+
+func _process(delta: float) -> void:
+    if abs(get_linear_velocity().x) > max_speed or abs(get_linear_velocity().y) > max_speed:
+        var new_speed = get_linear_velocity().normalized()
+        new_speed *= max_speed
+        set_linear_velocity(new_speed)
