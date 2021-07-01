0126136b101ae5975a103dda656782216cb99167
MomenaAkhter <makhter.shukhi@gmail.com>
2021-07-01 10:51:26 +0600

    Update indentation styles from tabs to spaces.
    Former-commit-id: dfc84ecf9d06b65f78d0e9f9104715bfa859ffa1

--- extensions/pong/scripts/Ball.gd
+++ extensions/pong/scripts/Ball.gd
@@ -9,38 +9,38 @@ var to_be_killed: bool = false
 
 
 func reset() -> void:
-	set_position(Vector2(512, 20))
-	set_linear_velocity(stage.ball_initial_velocity)
+    set_position(Vector2(512, 20))
+    set_linear_velocity(stage.ball_initial_velocity)
 
-	if stage.ball_harmful and !harmful:
-		toggle_harmful()
+    if stage.ball_harmful and !harmful:
+        toggle_harmful()
 
 
 func toggle_harmful() -> void:
-	harmful = !harmful
-	if harmful:
-		set_modulate(Color(1, .392, .392, 1))
-	else:
-		set_modulate(Color(1, 1, 1, 1))
+    harmful = !harmful
+    if harmful:
+        set_modulate(Color(1, .392, .392, 1))
+    else:
+        set_modulate(Color(1, 1, 1, 1))
 
 
 func _on_Ball_body_entered(body: Node) -> void:
-	if body.is_in_group("paddles"):
-		if harmful:
-			body.penalize_ball_hit()
-		else:
-			body.reward_ball_hit()
+    if body.is_in_group("paddles"):
+        if harmful:
+            body.penalize_ball_hit()
+        else:
+            body.reward_ball_hit()
 
 
 func _on_Ball_body_exited(body: Node) -> void:
-	# Escaping from a paddle collision
-	if !to_be_killed and (body.is_in_group("paddles") or body.is_in_group("harmful_boundaries")):
-		stage.reward_all_paddles()
-		to_be_killed = true
+    # Escaping from a paddle collision
+    if !to_be_killed and (body.is_in_group("paddles") or body.is_in_group("harmful_boundaries")):
+        stage.reward_all_paddles()
+        to_be_killed = true
 
 
 func kill() -> void:
-	if get_parent() != null:
-		to_be_killed = false
-		stage.remove_child(self)
-		
+    if get_parent() != null:
+        to_be_killed = false
+        stage.remove_child(self)
+
--- extensions/pong/scripts/parallel_evolution/Main.gd
+++ extensions/pong/scripts/parallel_evolution/Main.gd
@@ -11,100 +11,100 @@ enum {LEFT = 0, CENTER = 1, RIGHT = 2}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Fetch CLI arguments
-	var args = OS.get_cmdline_args()
-	configuration.parse(args[args.size() - 1])
+    # Fetch CLI arguments
+    var args = OS.get_cmdline_args()
+    configuration.parse(args[args.size() - 1])
 
-	# Prepare the population
-	$Neat.prepare(
-		configuration.population_size,
-		configuration.delta_disjoint,
-		configuration.delta_weights,
-		configuration.delta_threshold,
-		configuration.stale_species,
-		configuration.connection_mutate_chance,
-		configuration.perturb_chance,
-		configuration.crossover_chance,
-		configuration.link_mutation_chance,
-		configuration.node_mutation_chance,
-		configuration.bias_mutation_chance,
-		configuration.disable_mutation_chance,
-		configuration.enable_mutation_chance
-	)
+    # Prepare the population
+    $Neat.prepare(
+        configuration.population_size,
+        configuration.delta_disjoint,
+        configuration.delta_weights,
+        configuration.delta_threshold,
+        configuration.stale_species,
+        configuration.connection_mutate_chance,
+        configuration.perturb_chance,
+        configuration.crossover_chance,
+        configuration.link_mutation_chance,
+        configuration.node_mutation_chance,
+        configuration.bias_mutation_chance,
+        configuration.disable_mutation_chance,
+        configuration.enable_mutation_chance
+    )
 
-	# Scale the HUD
-	$Hud.set_scale(Vector2($Camera2D.zoom.x, $Camera2D.zoom.y))
+    # Scale the HUD
+    $Hud.set_scale(Vector2($Camera2D.zoom.x, $Camera2D.zoom.y))
 
-	# Add the balls
-	var ball_scene = preload("res://scenes/Ball.tscn")
-	for stage in $Stages.get_children():
-		var ball = ball_scene.instance()
-		ball.stage = stage
-		ball.reset()
-		stage.add_ball(ball)
+    # Add the balls
+    var ball_scene = preload("res://scenes/Ball.tscn")
+    for stage in $Stages.get_children():
+        var ball = ball_scene.instance()
+        ball.stage = stage
+        ball.reset()
+        stage.add_ball(ball)
 
-	# Add the paddles
-	var paddle_group_scene = preload("res://scenes/PaddleGroup.tscn")
-	var paddle_scene = preload("res://scenes/Paddle.tscn")
+    # Add the paddles
+    var paddle_group_scene = preload("res://scenes/PaddleGroup.tscn")
+    var paddle_scene = preload("res://scenes/Paddle.tscn")
 
-	for i in range(configuration.population_size):
-		var paddle_group: Node = paddle_group_scene.instance()
-		population.append(paddle_group)
+    for i in range(configuration.population_size):
+        var paddle_group: Node = paddle_group_scene.instance()
+        population.append(paddle_group)
 
-		# Add the paddle to each scene
-		for stage in $Stages.get_children():
-			var paddle: Node2D = paddle_scene.instance()
-			paddle.id = i
-			paddle_group.add(paddle)
-			stage.add_child(paddle)
-			paddle.reposition(CENTER)
+        # Add the paddle to each scene
+        for stage in $Stages.get_children():
+            var paddle: Node2D = paddle_scene.instance()
+            paddle.id = i
+            paddle_group.add(paddle)
+            stage.add_child(paddle)
+            paddle.reposition(CENTER)
 
 
 func _process(_delta: float) -> void:
-	var move_to_next_generation = true
-	for stage in $Stages.get_children():
-		if stage.ball.to_be_killed:
-			stage.ball.kill()
-			continue
+    var move_to_next_generation = true
+    for stage in $Stages.get_children():
+        if stage.ball.to_be_killed:
+            stage.ball.kill()
+            continue
 
-		if stage.ball.get_parent() != null:
-			move_to_next_generation = false
+        if stage.ball.get_parent() != null:
+            move_to_next_generation = false
 
-	if move_to_next_generation:
-		next_generation()
+    if move_to_next_generation:
+        next_generation()
 
 
 func next_generation() -> void:
-	stages_complete = 0
+    stages_complete = 0
 
-	# Calculate the scores and max fitness
-	var scores = []
-	for group in population:
-		var score: float = 0
-		for member in group.members:
-			score += member.fitness
-			member.fitness = 0
-			member.reposition(CENTER)
+    # Calculate the scores and max fitness
+    var scores = []
+    for group in population:
+        var score: float = 0
+        for member in group.members:
+            score += member.fitness
+            member.fitness = 0
+            member.reposition(CENTER)
 
-		if group.members.size() > 0:
-			$Neat.set_fitness(group.members[0].id, score)
-			scores.append(score)
-			max_fitness = max(score, max_fitness)
+        if group.members.size() > 0:
+            $Neat.set_fitness(group.members[0].id, score)
+            scores.append(score)
+            max_fitness = max(score, max_fitness)
 
-	scores.sort()
+    scores.sort()
 #    var message = ''
 #    for i in configuration.population_size:
 #        message += str(scores[configuration.population_size - i - 1]) + ' '
 #    print(message, '\n')
 
-	generation_id += 1
-	$Hud.set_generation(generation_id)
-	$Hud.set_max_fitness(max_fitness)
+    generation_id += 1
+    $Hud.set_generation(generation_id)
+    $Hud.set_max_fitness(max_fitness)
 
-	$Neat.next_generation()
+    $Neat.next_generation()
 
-	for stage in $Stages.get_children():
-		stage.revive_ball()
+    for stage in $Stages.get_children():
+        stage.revive_ball()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
