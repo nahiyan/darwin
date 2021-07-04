b982cbce99b2aaf00968f01c0bcd6a6149b79d15
MomenaAkhter <makhter.shukhi@gmail.com>
2021-07-04 16:29:03 +0600

    Reorganize toolchains directory.
    Former-commit-id: f2c54f40944945844e150a6148baff08f0963e6a

--- extensions/pong/modules/evolution/build.py
+++ extensions/pong/modules/evolution/build.py
@@ -1,7 +1,7 @@
 import os
 import sys
 if len(sys.argv) == 2 and (sys.argv[1] == "windows-x86_64" or sys.argv[1] == "windows"):
-    os.system("cmake . -DCMAKE_BUILD_TYPE=Release -Bbuild-windows-x86_64 -GNinja -DCMAKE_TOOLCHAIN_FILE=../../../../toolchains/windows-x86_64/CMakeLists.txt; cmake --build build-windows-x86_64")
+    os.system("cmake . -DCMAKE_BUILD_TYPE=Release -Bbuild-windows-x86_64 -GNinja -DCMAKE_TOOLCHAIN_FILE=../../../../toolchains/windows-x86_64.cmake; cmake --build build-windows-x86_64")
 else:
     os.system(
         "cmake . -DCMAKE_BUILD_TYPE=Release -Bbuild -GNinja; cmake --build build")
