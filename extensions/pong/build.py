956f5623b2060ed8a33bd855cd38402597b31931
MomenaAkhter <makhter.shukhi@gmail.com>
2021-07-19 16:26:47 +0600

    Add libraries for Linux.

--- extensions/pong/build.py
+++ extensions/pong/build.py
@@ -1,3 +1,3 @@
 import os
 os.system(
-    "cd godot-cpp; scons generate_bindings=yes -j16; cd ../modules/evolution; python build.py")
+    "cd godot-cpp; scons generate_bindings=yes target=release -j16; cd ../modules/evolution; python build.py")
